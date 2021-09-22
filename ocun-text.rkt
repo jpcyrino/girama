653594e26700739b3d9025ee0711d83a754cb976
jpcyrino <jpcyrino@gmail.com>
2021-09-22 18:26:07 -0300

    Ocun text update

--- ocun-text.rkt
+++ ocun-text.rkt
@@ -1,11 +1,49 @@
 #lang racket
 
-(define (text-file-line-to-list f)
-  (with-handlers ([exn:fail? (lambda (e) 'nao_pode_abrir_arquivo)])
+(provide ocun-text)
+
+
+(define (ocun-text txt-file)
+  (let ([sentence-vector (text-file-line-to-vector txt-file)])
+    (vector-multiple-of-3? sentence-vector)
+    (vector-to-sentence-struct sentence-vector)))
+
+;; Gets lines from a text file and transforms to vector
+(define (text-file-line-to-vector f)
+  (with-handlers ([exn:fail? (lambda (e) 'file_error)])
     (with-input-from-file f
       (lambda ()
-        (for/list ([line (in-lines)])
+        (for/vector ([line (in-lines)])
           line)))))
 
-(define (multiple-of-3 n)
+
+;; Transforms a vector of sentences into a list of sentence structs
+(define (vector-to-sentence-struct v)
+  (let loop ([acc 0]
+             [n (vector-length v)]
+             [l '()])
+    (if (< n 1)
+        (reverse l)
+        (loop (+ acc 3) (- n 3)
+              (cons
+               (sentence
+                (vector-ref v acc) (vector-ref v (+ acc 1)) (vector-ref v (+ acc 2))) l)))))
+
+(module+ test
+  (require rackunit)
+  (check-equal? (vector-to-sentence-struct '#(1 2 3 4 5 6))
+                (list (sentence 1 2 3) (sentence 4 5 6))))
+
+
+;; Auxiliary functions
+(define (vector-multiple-of-3? l)
+  (when (not (multiple-of-3? (vector-length l)))
+    (error "Incomplete data")))
+
+(define (multiple-of-3? n)
   (= (remainder n 3) 0))
+
+(struct sentence (language gloss translation) #:transparent)
+
+
+
